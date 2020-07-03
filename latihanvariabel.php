1162e9d700e15e99995f9661930a047e7d6f257e
dwi <dwirianawijaya@gmail.com>
2020-07-03 08:01:53 +0700

    latihanvariabel1.php

---
+++ latihanvariabel.php
@@ -0,0 +1,17 @@
+<?php 
+
+//variabel a
+$a = "Toni Stark";
+$b = "spidermen";
+
+echo $a;
+echo "<br>";
+echo $b;
+echo "<br>";
+echo "$a";
+echo "<br>";
+
+// penulisan pada petik satu yang akan ditampilan tidak akan muncul
+echo '$b'; 
+
+?>
